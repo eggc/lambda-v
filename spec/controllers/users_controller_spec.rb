abdba0432f70f41ba1f6204b6b1e4bae3f7d5f94
Ken Eguchi <no.eggchicken@gmail.com>
2018-11-16 00:04:05 +0900

    Use arbitrary helper methods

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,23 +7,19 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "POST #create" do
-    context "with valid params" do
-      before do
-        params = { user: { email: 'alice@example.com', password: '3bt2[J!' } }
-        post(:create, params: params)
-      end
+    def post_with(user_params)
+      post(:create, params: { user: user_params })
+    end
 
+    context "with valid params" do
+      before { post_with(email: 'alice@example.com', password: '3bt2[J!') }
       it { expect(response).to redirect_to(root_path) }
       it { expect(User.count).to eq(1) }
       it { expect(User.first.email).to eq('alice@example.com') }
     end
 
-    context "invalid params" do
-      before do
-        params = { user: { email: 'alice@example.com', password: nil } }
-        post(:create, params: params)
-      end
-
+    context "without password" do
+      before { post_with(email: 'alice@example.com', password: nil) }
       it { expect(response).to have_http_status(:success) }
       it { expect(User.count).to eq(0) }
     end
