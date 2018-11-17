0f84fec14311c0742c3e0078d9542e6fcf57bbd3
Ken Eguchi <no.eggchicken@gmail.com>
2018-11-17 21:38:02 +0900

    Add locale to top scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'root#index'
-  resources :users, only: [:new, :create]
-  resource :sessions, only: [:new, :create, :destroy]
-  get 'home', to: 'home#index'
+  scope "(:locale)", locale: /en|ja/ do
+    root to: 'root#index'
+    resources :users, only: [:new, :create]
+    resource :sessions, only: [:new, :create, :destroy]
+    get 'home', to: 'home#index'
+  end
 end
