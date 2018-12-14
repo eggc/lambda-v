78ae21e8f38a24ed52947d41d4f2f09829cd736d
Ken Eguchi <eguchi@haw.co.jp>
2018-12-14 09:49:19 +0900

    Add spec for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,4 +24,34 @@ RSpec.describe UsersController, type: :controller do
       it { expect(User.count).to eq(0) }
     end
   end
+
+  describe 'PATCH #update' do
+    before do
+      @user = User.create(email: 'alice@example.com', password: '-30vfa')
+      @update_params = { id: @user.id, user: { email: 'new@example.com', password: 'xyz' }}
+      @invalid_params = { id: @user.id, user: { email: '', password: '' }}
+    end
+
+    context 'with login' do
+      before { controller.auto_login(@user) }
+
+      context 'valid params' do
+        before { patch(:update, params: @update_params) }
+        it { expect(response).to redirect_to(root_path) }
+        it { expect(@user.reload.email).to eq('new@example.com') }
+      end
+
+      context 'invalid params' do
+        before { patch(:update, params: @invalid_params) }
+        it { expect(response).to have_http_status(:success) }
+        it { expect(@user.reload.email).to eq('alice@example.com') }
+      end
+    end
+
+    context 'without login' do
+      before { patch(:update, params: @update_params) }
+      it { expect(response).to redirect_to(root_path) }
+      it { expect(@user.reload.email).to eq('alice@example.com') }
+    end
+  end
 end
