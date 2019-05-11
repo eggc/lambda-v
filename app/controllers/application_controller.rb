edf18e552e8c5d5414ab9b23eee043b98ead4cd8
Ken Eguchi <no.eggchicken@gmail.com>
2019-05-11 17:57:54 +0900

    Use browser locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,16 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || browser_locale || I18n.default_locale
   end
 
   def default_url_options
     { locale: I18n.locale }
+  end
+
+  private
+
+  def browser_locale
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
