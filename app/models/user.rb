a1f19009e71555a8d8262e33df67de5d4c6b0728
Ken Eguchi <no.eggchicken@gmail.com>
2019-05-11 20:40:35 +0900

    Use twitter original size image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_one_attached :icon
@@ -11,8 +13,17 @@ class User < ApplicationRecord
   before_save :attach_icon
 
   # 予めセットされていた icon_url からデータを取ってきて active storage に icon を保存する
+  # ツイッターから取得した icon の場合は xxx_normal.jpg のようなファイル名になっており
+  # オリジナル画像よりかなり小さい。これを避けるためファイル末尾の _normal を削除する。
   def attach_icon
-    downloaded_image = open(icon_url)
-    icon.attach(io: downloaded_image, filename: File.basename(icon_url))
+    if icon_url.start_with?("https://pbs.twimg.com/profile_images")
+      extname = File.extname(icon_url)
+      url = icon_url.sub("_normal#{extname}", extname)
+    else
+      url = icon_url
+    end
+
+    downloaded_image = open(url)
+    icon.attach(io: downloaded_image, filename: File.basename(url))
   end
 end
