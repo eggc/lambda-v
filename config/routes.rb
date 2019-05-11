d3b9f81df8b74368a161271b755b7dc8d6934763
Ken Eguchi <no.eggchicken@gmail.com>
2019-05-11 13:48:09 +0900

    Add google auth login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     resources :users, only: [:new, :create, :edit, :update]
     resource :sessions, only: [:new, :create, :destroy]
     get 'home', to: 'home#index'
+    post "oauth/callback" => "oauths#callback"
+    get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
+    get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   end
 end
