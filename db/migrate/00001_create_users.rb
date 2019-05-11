0208fb62bca46f2097f9649d98dfa9f4ffce001b
Ken Eguchi <no.eggchicken@gmail.com>
2019-05-11 13:26:17 +0900

    Allow empty password for oauth user

--- db/migrate/00001_create_users.rb
+++ db/migrate/00001_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email, null: false
-      t.string :crypted_password, null: false
-      t.string :salt, null: false
+      t.string :crypted_password
+      t.string :salt
       t.timestamps
 
       t.index :email, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 2018_12_13_080935) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
-    t.string "crypted_password", null: false
-    t.string "salt", null: false
+    t.string "crypted_password"
+    t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
